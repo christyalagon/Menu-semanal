efbb0feffbb929d9f1b1e5c9a7c42e2561e91d53
christyalagon <45948285+christyalagon@users.noreply.github.com>
2026-09-23 12:15:16 +0200

    Actualiza la caché de la PWA

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mi-menu-v1';
+const CACHE = 'mi-menu-v2';
 const ASSETS = ['./', './index.html', './styles.css', './app.js', './core.js', './menu.js', './manifest.webmanifest', './icon.svg', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
@@ -8,5 +8,8 @@ self.addEventListener('activate', event => {
 });
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET' || new URL(event.request.url).origin !== self.location.origin) return;
-  event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request)));
+  event.respondWith(fetch(event.request).then(response => {
+    if (response.ok) caches.open(CACHE).then(cache => cache.put(event.request, response.clone()));
+    return response;
+  }).catch(() => caches.match(event.request).then(hit => hit || caches.match('./'))));
 });
